3347687c6fe69515f975d5c43975fefb9aa5f123
Daisuke Sato <daisukes@cmu.edu>
2023-08-08 22:52:48 -0400

    add -o option for unzip command
    Signed-off-by: Daisuke Sato <daisukes@cmu.edu>

--- download-lib.sh
+++ download-lib.sh
@@ -13,10 +13,10 @@ curl -O https://code.jquery.com/jquery-1.11.2.min.js
 cd ..
 
 curl -L -O https://jqueryui.com/resources/download/jquery-ui-1.11.4.zip
-unzip jquery-ui-1.11.4.zip
+unzip -o jquery-ui-1.11.4.zip
 
 curl -L -O https://github.com/DataTables/DataTables/archive/1.10.6.zip
-unzip 1.10.6.zip
+unzip -o 1.10.6.zip
 
 cd $pwd
 mkdir -p ./QueryService/WebContent/WEB-INF/lib
@@ -25,5 +25,5 @@ cd ./QueryService/WebContent/WEB-INF/lib
 curl -L -O http://central.maven.org/maven2/org/mongodb/mongo-java-driver/3.4.0/mongo-java-driver-3.4.0.jar
 
 curl -L -O http://archive.apache.org/dist/wink/1.4.0/apache-wink-1.4.zip
-unzip apache-wink-1.4.zip apache-wink-1.4/dist/wink-1.4.jar
+unzip -o apache-wink-1.4.zip apache-wink-1.4/dist/wink-1.4.jar
 mv apache-wink-1.4/dist/wink-1.4.jar ./
